6b300686cc55b90107678301d56a972275236669
hikam074 <hikam074@gmail.com>
2024-09-25 18:33:05 +0700

    Add project files.

---
+++ TUGAS1PRAKTIKUM/Classes/Hewan.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace TUGAS1PRAKTIKUM.Classes
+{
+    // ins NO. 2a : membuat class Hewan
+    public class Hewan
+    {
+        public string nama;
+        public int umur;
+
+        // ins method NO. 3a : suara Hewan
+        public virtual string Suara()
+        {
+            return "Hewan ini bersuara";
+        }
+        // ins method NO. 3b : menampilkan informasi hewan
+        public virtual string InfoHewan()
+        {
+            return $"INFO | Nama : {this.nama}, umur : {this.umur}";
+        }
+    }
+
+    // ins NO. 2b  : membuat derived class Mamalia dari Hewan
+    public class Mamalia : Hewan
+    {
+        public int jumlahKaki;
+
+        // ins method NO. 4 : override suara Hewan
+        public override string Suara()
+        {
+            return $"mamalia ini bersuara dengan keras!";
+        }
+        public override string InfoHewan()
+        {
+            return $"INFO | Nama : {this.nama}, umur : {this.umur}, jumlah kaki : {this.jumlahKaki}";
+        }
+    }
+
+    // ins NO. 2c : membuat derived class Reptil dari Hewan
+    public class Reptil : Hewan
+    {
+        public double panjangTubuh;
+
+        // ins method NO. 4 : override suara Hewan
+        public override string Suara()
+        {
+            return $"reptil ini bersuara dengan keras!";
+        }
+    }
+
+    // ins NO. 2d : membuat derived class Singa dari Mamalia
+    public class Singa : Mamalia
+    {
+        // ins method NO. 4 : override suara Hewan
+        public override string Suara()
+        {
+            return "Singa ini bersuara 'Aummm!'";
+        }
+        // ins method NO. 5.1 : method khusus Singa
+        public void Mengaum()
+        {
+            Console.WriteLine("RAWRR!! AUUMMM!");
+        }
+    }
+
+    // ins NO. 2d : membuat derived class Gajah dari Mamalia
+    public class Gajah : Mamalia
+    {
+        // ins method NO. 4 : override suara Hewan
+        public override string Suara()
+        {
+            return "Gajah ini bersuara  'treet'";
+        }
+    }
+
+    // ins NO. 2e : membuat derived class Ular dari Reptil
+    public class Ular : Reptil
+    {
+        // ins method NO. 4 : override suara Hewan
+        public override string Suara()
+        {
+            return "Ular ini bersuara 'Sssss'";
+        }
+        // ins method NO. 5b : method khusus Ular
+        public void Merayap()
+        {
+            Console.WriteLine("Ular ini merayap di tanah...");
+        }
+    }
+
+    // ins NO. 2e : membuat derived class Buaya dari Reptil
+    public class Buaya : Reptil
+    {
+        // ins method NO. 4 : override suara Hewan
+        public override string Suara()
+        {
+            return "Buaya ini bersuara 'grrrr'";
+        }
+    }
+    // ins NO. 2f : membuat class KebunBinatang
+    public class KebunBinatang
+    {
+        public List<Hewan> koleksiHewan = new List<Hewan>();
+        // ins method NO. 6a : menambahkan Hewan ke list
+        public void TambahHewan(Hewan hewan)
+        {
+            this.koleksiHewan.Add(hewan);
+        }
+        // ins method NO. 6b : menampilkan seluruh hewan yang ada di list
+        public void DaftarHewan()
+        {
+            foreach (Hewan animal in this.koleksiHewan)
+            {
+                Console.WriteLine(animal.InfoHewan());
+            }
+        }
+    }
+}
---
+++ TUGAS1PRAKTIKUM/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using TUGAS1PRAKTIKUM.Classes;
+
+namespace Praktikum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ins NO. 7a : membuat object KebunBinatang
+            KebunBinatang Prigen = new KebunBinatang();
+
+            // ins NO. 7b : membuat object dari tiap subkelas
+            Singa singa1 = new Singa();
+            singa1.nama = "Muliono";
+            singa1.umur = 52;
+            singa1.jumlahKaki = 4;
+
+            Gajah gajah1 = new Gajah();
+            gajah1.nama = "Budi";
+            gajah1.umur = 60;
+            gajah1.jumlahKaki = 3;
+
+            Ular ular1 = new Ular();
+            ular1.nama = "Wati";
+            ular1.umur = 70;
+            ular1.panjangTubuh = 2.8;
+
+            Buaya buaya1 = new Buaya();
+            buaya1.nama = "Yono";
+            buaya1.umur = 50;
+            buaya1.panjangTubuh = 2.1;
+
+            // ins NO. 7c : menambahkan kelas ke list KebunBinantang
+            Prigen.TambahHewan(buaya1);
+            Prigen.TambahHewan(ular1);
+            Prigen.TambahHewan(gajah1);
+            Prigen.TambahHewan(singa1);
+
+            // ins NO. 7d : menampilkan daftar hewan pada list
+            Prigen.DaftarHewan();
+
+            // ins NO. 7e : memanggil suara tiap hewan yang telah diimplementasikan polymorphism
+            singa1.Suara();
+            gajah1.Suara();
+            ular1.Suara();
+            buaya1.Suara();
+
+            // ins No. 7f : memanggil method khusus
+            singa1.Mengaum();
+            ular1.Merayap();
+
+            // BUFFER
+            Console.WriteLine();
+            Console.WriteLine();
+            // BUFFER
+
+            // ana NO. 1 : suara() gajah dan ular
+            gajah1.Suara();
+            ular1.Suara();
+
+            Console.WriteLine(gajah1.Suara());
+            Console.WriteLine(ular1.Suara());
+
+            // ana NO. 2 : mengaum() singa
+            singa1.Mengaum();
+
+            // ana NO. 3 : informasi lengkap
+            singa1.InfoHewan();
+
+            Console.WriteLine(singa1.InfoHewan());
+
+            // ana NO. 4 : merayap() ular
+            ular1.Merayap();
+
+            // ana NO. 5 : reptil -> buaya
+            Reptil reptil = new Buaya();
+            reptil.Suara();
+
+            Console.WriteLine(reptil.Suara());
+            Console.WriteLine(ular1);
+        }
+    }
+}
